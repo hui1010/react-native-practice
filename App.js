ec94c73cde6bd26139b56588167381dda4308790
Huiyi Skårner <63238749+hui1010@users.noreply.github.com>
2021-07-17 00:03:15 +0200

    Text, View and Button

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Linking } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello Huiyi!</Text>
+      <Button title='Click me' onPress={()=>{Linking.openURL('https://github.com/')}}></Button>
       <StatusBar style="auto" />
     </View>
   );
