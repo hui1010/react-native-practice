19ffb641af9d09d92c9cd26bf6bb733fd1094a05
Huiyi Skårner <63238749+hui1010@users.noreply.github.com>
2021-07-17 00:22:20 +0200

    useState hook

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, Linking } from 'react-native';
 
 export default function App() {
+  const [number, setNumber] = useState(0)
+  const handlePress = () => {
+    setNumber(number + 5)
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Hello Huiyi!</Text>
-      <Button title='Click me' onPress={()=>{Linking.openURL('https://github.com/')}}></Button>
+      <Text>{number}</Text>
+      {/* <Button title='Click me' onPress={()=>{Linking.openURL('https://github.com/')}}></Button> */}
+      <Button title='add' onPress={handlePress}></Button>
       <StatusBar style="auto" />
     </View>
   );
